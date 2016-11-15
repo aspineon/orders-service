fd3814d8d26fb2682acafe406b0a4cb3149be545
Christian Posta <christian.posta@gmail.com>
2016-11-15 08:51:19 -0700

    update to latest jenkinsfile

--- cleanup.sh
+++ cleanup.sh
@@ -3,7 +3,9 @@
 # delete app stuff
 oc delete is/orders-service-mysql
 oc delete bc/orders-service-mysql-s2i
+oc delete dc/orders-service-mysql
+oc delete svc/orders-service-mysql
 
 # delete database stuff
 oc delete svc/mysqlorders
-
+oc delete deployment/mysqlorders
